5dbafa1607bfc3257b95a47ec4de7b07e23e7cf2
ismayilzeynal <ismayilbz@code.edu.az>
2022-08-02 13:02:23 +0400

    c# intro created

---
+++ task (1)/task (1)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 6, num2 = 14, result;            // 8-11  7-14 6-9 10-14  7-10  6-15   6-14
+            result = num2 / 7 - num1 / 7;               //  0     2    1    1     1     2      2
+            if (num1 % 7 == 0)
+                Console.WriteLine(result + 1);
+            else
+                Console.WriteLine(result);
+        }
+    }
+}
---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "february";
+            if (month == "february")
+                Console.WriteLine("28 or 29");
+            // bir dene | (or) isaresi qoyduqda uygunluq tapdiqda digerlerinin uzerinden bosuna kecmir. Yeni zamana qenaet edir.
+            else if (month == "april" | month == "june" | month == "september" | month == "november")       
+                Console.WriteLine(30);
+            else if (month == "january" | month == "march" | month == "may" | month == "july" | month == "august" | month == "october" | month == "december")
+                Console.WriteLine(31);
+            else
+                Console.WriteLine("wrong input. Please rewrite month name");
+        }
+    }
+}
+
